2f15d91c0217a444149ddb10f7896b7ca59a83c6
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-10-24 14:35:13 +0300

    Additional time adjusting

--- src/server/services/various.js
+++ src/server/services/various.js
@@ -1,13 +1,13 @@
 require('dotenv').config()
 
 function fDateTime(local, time = new Date()) {
-  const DELTA_RUBY_TIME_ZONE_MINUTES = Number(process.env.DELTA_RUBY_TIME_ZONE_MINUTES) || 0
+  const DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES = Number(process.env.DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES) || 0
   let adjustedTime = new Date(time)
 
   if (isNaN(adjustedTime)) {
     adjustedTime = new Date()
   }
-  adjustedTime = new Date(adjustedTime.getTime() - DELTA_RUBY_TIME_ZONE_MINUTES * 60000)
+  adjustedTime = new Date(adjustedTime.getTime() - DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES * 60000)
 
   const currentTime = adjustedTime.toLocaleString(local, { dateStyle: 'medium', timeStyle: 'short' })
   return currentTime
