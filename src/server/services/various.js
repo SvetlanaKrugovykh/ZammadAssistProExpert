f401fb0d988c23f21939db3f16d38bd9201d578e
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-10-25 10:11:34 +0300

    some changes

--- src/server/services/various.js
+++ src/server/services/various.js
@@ -1,13 +1,14 @@
 require('dotenv').config()
 
-function fDateTime(local, time = new Date()) {
-  const DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES = Number(process.env.DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES) || 0
+function fDateTime(local, time = new Date(), isDelta = false) {
+  const DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES = Number(process.env.DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES) * 60000 || 0
+  const DELTA_ADD = isDelta ? Number(process.env.DELTA_ADD) * 60000 : 0
   let adjustedTime = new Date(time)
 
   if (isNaN(adjustedTime)) {
     adjustedTime = new Date()
   }
-  adjustedTime = new Date(adjustedTime.getTime() - DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES * 60000)
+  adjustedTime = new Date(adjustedTime.getTime() - DELTA_RUBY_TIME_FOR_MESSAGE_MINUTES + DELTA_ADD)
 
   const currentTime = adjustedTime.toLocaleString(local, { dateStyle: 'medium', timeStyle: 'short' })
   return currentTime
