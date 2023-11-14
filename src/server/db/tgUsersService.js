34453583aeaf000eb49e22b3cf573243035a9e4c
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-11-14 22:12:05 +0100

    some fixes

--- src/server/db/tgUsersService.js
+++ src/server/db/tgUsersService.js
@@ -55,7 +55,7 @@ async function createOrUpdateUserIntoDb(chatId, user_info) {
     if (!existingUser) existingUser = await findUserByEmail(email_.replace(/\s+/g, ''))
     if (DEBUG_LEVEL > 0) console.log(`existingUser: ${JSON.stringify(existingUser)}`)
     if (existingUser) {
-      const query = 'UPDATE users SET login = $1, phone = $2, firstname = $3, lastname = $4, email = $5, source = $6 WHERE user_id = $7 RETURNING *'
+      const query = 'UPDATE users SET login = $1, phone = $2, firstname = $3, lastname = $4, email = $5, source = $6 WHERE id = $7 RETURNING *'
       const values = [
         chatId,
         user_info.phoneNumber,
