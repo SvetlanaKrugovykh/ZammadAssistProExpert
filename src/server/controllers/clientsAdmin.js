14c5e2556ecf384f00a77e338b6327a3be064ec9
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2023-11-15 10:50:50 +0100

    await new Promise(resolve => setTimeout(resolve, 1000))

--- src/server/controllers/clientsAdmin.js
+++ src/server/controllers/clientsAdmin.js
@@ -64,6 +64,7 @@ async function sendInfoAboutApproveRegistration(bot, user_tgID) {
 
 async function sendInfoAboutDeclineRegistration(bot, user_tgID, reason = 'не вказано') {
   try {
+    await new Promise(resolve => setTimeout(resolve, 1500))
     await bot.sendMessage(user_tgID, `Вашу заявку на реєстрацію в системі <b>Інтерактивний чат-бот</b> було відхилено з причини ${reason}. Для отримання додаткової інформації зверніться до адміністратора системи.`, { parse_mode: 'HTML' })
   } catch (err) {
     console.log(err)
