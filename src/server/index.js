774b7e66f855a9b1317e86116b998ffeeede416f
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-02-20 13:00:21 +0100

    added cors to 8001

--- src/server/index.js
+++ src/server/index.js
@@ -44,6 +44,7 @@ cron.schedule(TICKET_AUTO_CLOSE_SCHEDULLER_STRING, () => {
 })
 
 app.register(require('@fastify/cors'), {})
+interConnectApp.register(require('@fastify/cors'), {})
 
 interConnectApp.register(require('./routes/interConnect.route'), { prefix: '/inter-connect' })
 
