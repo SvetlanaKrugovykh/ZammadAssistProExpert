a07225b992afecf85f25c23a333599067c78454e
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2024-10-11 13:32:05 +0200

    finished _ADD_HOUR

--- src/server/services/scheduledTasks.js
+++ src/server/services/scheduledTasks.js
@@ -8,15 +8,13 @@ require('dotenv').config()
 
 async function checkAndReplaceTicketsStatuses(bot) {
   try {
-    const DELTA_RUBY_TIME_ZONE_MINUTES = Number(process.env.DELTA_RUBY_TIME_ZONE_MINUTES) || 0
     const TICKET_AUTO_CLOSE_DAYS = Number(process.env.TICKET_AUTO_CLOSE_DAYS) || 3
     let INTERVAL_MINUTES = Number(process.env.CLOSED_TICKET_SCAN_INTERVAL_MINUTES_FOR_DB) || 11
     if (process.env.ZAMMAD_USER_TEST_MODE === 'true') INTERVAL_MINUTES = Number(process.env.CLOSED_TICKET_SCAN_INTERVAL_MINUTES_FOR_TEST) || 10
-    const DELTA_SUMMER_TIME = Number(process.env.DELTA_SUMMER_TIME) || 0
-    const ANOTHER_DELTA = 60000 * 60 * (Number(process.env.ANOTHER_DELTA) || 2)
 
-    const DELTA = (DELTA_RUBY_TIME_ZONE_MINUTES + INTERVAL_MINUTES + DELTA_SUMMER_TIME) * 60000
-    const nowMinusInterval = new Date(Date.now() - DELTA + ANOTHER_DELTA)
+    const hoursToAdd = Number(process.env.HOURS_TO_ADD) || 5
+    const DELTA = ((hoursToAdd * 60) + INTERVAL_MINUTES) * 60000
+    const nowMinusInterval = new Date(Date.now() - DELTA)
 
     const exceptHour = Number(process.env.EXCEPT_HOUR) || 4
     const query = `SELECT * FROM tickets WHERE state_id = 4 AND pending_time IS NULL AND updated_at > $1`
