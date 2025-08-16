ef325b9264fee1cf225d7f563f88801bc39e2477
SvetlanaKrugovykh <Svetlana.Krugovykh@gmail.com>
2025-08-16 15:54:13 +0200

    API +

--- src/server/services/tickets-api.js
+++ src/server/services/tickets-api.js
@@ -1,40 +1,26 @@
 const https = require('https')
 const axios = require('axios')
+const { findUserById } = require('../db/tgUsersService')
 require('dotenv').config()
 
 async function createTicket(ticketData) {
   try {
-    const headers = {
-      Authorization: `Bearer ${process.env.ZAMMAD_API_TOKEN}`,
-      "Content-Type": "application/json"
-    }
-
+    const user = await findUserById(ticketData.customer_id)
     const data = {
       title: ticketData.title,
       group_id: ticketData.group_id || 1,
-      customer_id: ticketData.customer_id,
+      customer_id: user.id,
       priority_id: ticketData.priority_id || 2,
       state_id: ticketData.state_id || 1,
       article: {
         subject: ticketData.title,
         body: ticketData.body,
-        type: ticketData.article_type || 'note',
-        internal: ticketData.internal || false
+        type: ticketData?.article_type || 'note',
+        internal: ticketData?.internal || false
       }
     }
 
-    if (ticketData.owner_id) {
-      data.owner_id = ticketData.owner_id
-    }
-
-    console.log('Creating ticket with data:', JSON.stringify(data, null, 2))
-
-    const httpsAgent = new https.Agent({ rejectUnauthorized: false })
-    const url = `${process.env.ZAMMAD_API_URL}/tickets`
-
-    const response = await axios.post(url, data, { headers, httpsAgent })
-    const ticket = response.data
-
+    const ticket = await create_api_ticket(data)
     console.log(`Ticket created successfully: ${ticket.id}`)
     return ticket
 
@@ -47,6 +33,25 @@ async function createTicket(ticketData) {
     throw error
   }
 }
+
+
+async function create_api_ticket(data) {
+  const headers = { Authorization: process.env.ZAMMAD_API_TOKEN, "Content-Type": "application/json" }
+  console.log('Data to be sent:', JSON.stringify(data, null, 2))
+
+  const httpsAgent = new https.Agent({ rejectUnauthorized: false })
+  const url = `${process.env.ZAMMAD_API_URL}/tickets`
+  try {
+    const response = await axios.post(url, data, { headers, httpsAgent })
+    const ticket = response.data
+    console.log(`Crete ticket: ${ticket.id}`)
+    return ticket
+  } catch (err) {
+    console.log(err)
+    return null
+  }
+}
+
 
 module.exports = {
   createTicket
